7be28c2e8b1c28e8f72a330e4230814f38efa52b
0T001Z <tzmixsuporte@gmail.com>
2025-04-25 03:02:14 -0300

    Delete css directory

--- css/admin/save.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $dados = json_encode($_POST) . PHP_EOL;
-    file_put_contents("dados.txt", $dados, FILE_APPEND);
-    header("Location: painel.php");
-}
-?>
